1f86c2991cd18a5a6291b519eff07b2105aabc36
raunakhajela <raunakhajela@gmail.com>
2025-10-13 21:14:25 +0530

    implement color picker

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import { useCollection, useFirestore, useCurrentUser } from 'vuefire'
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
-import { Plus } from 'lucide-vue-next'
+import { Plus, Minus } from 'lucide-vue-next'
 import AddList from './AddList.vue'
 import ListItem from './ListItem.vue'
 
@@ -28,15 +28,9 @@ async function handleAddList({ name, color }) {
 
     const docRef = await addDoc(collection(db, 'lists'), newList)
     console.log("List created with ID: ", docRef.id)
-
-    toggleAddList.value = false
   } catch (e) {
     console.error("Error adding list: ", e)
   }
-}
-
-function handleCancel() {
-  toggleAddList.value = false
 }
 
 function handleListClick(list) {
@@ -45,32 +39,59 @@ function handleListClick(list) {
 </script>
 
 <template>
-  <div class="w-64 h-full flex flex-col gap-4">
+  <div class="w-64 h-full flex flex-col gap-4 flex-shrink-0 overflow-hidden">
     <!-- Header -->
-    <div class="flex items-center justify-between">
+    <div class="flex items-center justify-between flex-shrink-0">
       <h2 class="text-lg font-semibold text-gray-900">Lists</h2>
       <Plus
+        v-if="!toggleAddList"
         size="18"
         @click="toggleAddList = !toggleAddList"
-        class="cursor-pointer"
+        class="cursor-pointer hover:text-gray-600 transition-colors"
+      />
+      <Minus
+        v-else
+        size="18"
+        @click="toggleAddList = !toggleAddList"
+        class="cursor-pointer hover:text-gray-600 transition-colors"
       />
     </div>
 
-    <!-- Add List Form -->
-    <AddList
-      v-if="toggleAddList"
-      @add-list="handleAddList"
-      @cancel="handleCancel"
-    />
+    <transition
+      enter-active-class="transition-all duration-200 ease-out"
+      enter-from-class="opacity-0 transform -translate-y-2"
+      enter-to-class="opacity-100 transform translate-y-0"
+      leave-active-class="transition-all duration-150 ease-in"
+      leave-from-class="opacity-100 transform translate-y-0"
+      leave-to-class="opacity-0 transform -translate-y-2"
+    >
+      <div v-if="toggleAddList" class="flex-shrink-0">
+        <AddList @add-list="handleAddList" />
+      </div>
+    </transition>
 
     <!-- Lists -->
-    <ul class="flex flex-col gap-2">
-      <ListItem
-        v-for="list in lists"
-        :key="list.id"
-        :list="list"
-        @click="handleListClick"
-      />
-    </ul>
+    <div class="flex-1 overflow-y-auto overflow-x-hidden min-h-0 custom-scrollbar">
+      <ul class="flex flex-col gap-2 pr-2">
+        <ListItem
+          v-for="list in lists"
+          :key="list.id"
+          :list="list"
+          @click="handleListClick"
+        />
+      </ul>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.custom-scrollbar::-webkit-scrollbar {
+  width: 0px;
+  display: none;
+}
+
+.custom-scrollbar {
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+</style>
